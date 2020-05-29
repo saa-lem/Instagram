45fa6eabd7737e481525d2b53b77f5cfb3de78c9
Salem Owino <salemowino18@gmail.com>
2020-05-29 17:55:01 +0300

    An image model created

--- instagram/models.py
+++ instagram/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
+from django.contrib.auth.models import User
+from users.models import Profile
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='media/uploads/', default='media/default.jpg')
+    image_name = models.CharField(max_length=60)
+    image_caption = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, blank = True, related_name ='image_likes')
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def get_absolute_url(self):
+        return reverse('image-detail',kwargs = {'pk':self.pk} )
