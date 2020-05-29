09fb2dce7d5417acd43d3a766bcb930a62cabaae
Salem Owino <salemowino18@gmail.com>
2020-05-29 17:23:01 +0300

    Installed apps included

--- instagram_project/settings.py
+++ instagram_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'instagram.apps.InstagramConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
